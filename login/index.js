e769cfbe2d471bb42e3824931b141b220b2b6cab
colehallberg <hallbergc@g.cofc.edu>
2024-10-09 21:14:25 -0400

    Added JS files to take user input and store it

---
+++ login/index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("login_user");
+  
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const email = document.getElementById("user_email").value;
+      const password = document.getElementById("user_password").value;
+  
+      // Temporary for testing
+      console.log("Email: ", email);
+      console.log("Password:", password);
+    });
+  });
+  
---
+++ register/index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("register_user");
+  
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const firstname = document.getElementById("user_firstname").value;
+      const lastname = document.getElementById("user_lastname").value;
+      const email = document.getElementById("user_email").value;
+      const password = document.getElementById("user_password").value;
+      const passwordredo = document.getElementById("user_password_redo").value;
+  
+      // Temporary for testing
+      console.log("Name: ", firstname + lastname);
+      console.log("Email: ", email);
+      console.log("Password:", password);
+      console.log("Password Redo:", passwordredo);
+    });
+  });
+  
