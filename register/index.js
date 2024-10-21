badec9efbd53c38fb11f993f701fdf0119212c28
colehallberg <hallbergc@g.cofc.edu>
2024-10-21 18:57:17 -0400

    added user role selection, add job, and profile creation

--- register/index.js
+++ register/index.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById("register_user");
-  
-    form.addEventListener("submit", (e) => {
+document.addEventListener("DOMContentLoaded", function() { 
+  const form = document.getElementById("register_user");
+
+  form.addEventListener("submit", (e) => {
       e.preventDefault();
 
       const firstname = document.getElementById("user_firstname").value;
@@ -9,13 +9,30 @@ document.addEventListener("DOMContentLoaded", function() {
       const email = document.getElementById("user_email").value;
       const password = document.getElementById("user_password").value;
       const passwordredo = document.getElementById("user_password_redo").value;
-  
-      // Temporary for testing
-      console.log("Name: ", firstname + lastname);
+      const selectedRole = document.querySelector('input[name="role"]:checked').value;
+
+      //temporary for testing
+      console.log("Name: ", firstname + ' ' + lastname);
       console.log("Email: ", email);
       console.log("Password:", password);
       console.log("Password Redo:", passwordredo);
-      location.href='role_specification.html';
-    });
+      console.log("Selected Role:", selectedRole);
+
+      document.getElementById('selected-role').innerText = selectedRole;
+
+      document.querySelector('.formContainer').style.display = 'none';
+      document.getElementById('notification-box').style.display = 'block';
   });
-  
+});
+
+function returnToRegistration() {
+  document.getElementById('notification-box').style.display = 'none';
+  document.querySelector('.formContainer').style.display = 'block';
+}
+
+function displayView(userRole){
+  if (userRole === "Client") {
+    window.location.href = "../clientFunctionality/welcome_client.html";
+} else if (userRole === "Contractor") {
+    window.location.href = "../contractorFunctionality/welcome_contractor.html";
+}}
